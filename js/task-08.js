06bdcf95c18fc00c05a8b397558653999b8960fa
HannaMyrhorod <hanna.mirgorod@gmail.com>
2022-05-24 17:50:13 +0300

    Додає рішеня завдань #8-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (email === '' || password === '') {
+       return alert(`All fields are required!`);
+    };
+
+    const formData = { email, password, };
+    console.log(formData);
+    
+    event.currentTarget.reset();
+
+    // // Using class FormData
+    // const formData = new FormData(event.currentTarget);
+    // console.log(formData);
+    // formData.forEach((value, name) => {
+    //     console.log(`${name}: ${value}`);
+    // })
+};
+ 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const refs = {
+  changeColorBtn: document.querySelector('.change-color'),
+  colorName: document.querySelector('.color'),
+  body: document.querySelector('body')
+}
+
+refs.changeColorBtn.addEventListener('click', onChangeColorBtnClick);
+
+function onChangeColorBtnClick() {
+  refs.colorName.textContent = getRandomHexColor();
+  refs.body.style.backgroundColor = refs.colorName.textContent;
+}
